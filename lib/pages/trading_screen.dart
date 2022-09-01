c0c0fc72cfc06bcf9776ca17ec50b392f248d924
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-09-02 06:13:43 +0700

    added background to trading screen

--- lib/pages/trading_screen.dart
+++ lib/pages/trading_screen.dart
@@ -1,10 +1,30 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:rmdoo_salesforce/widgets/trading/trading_icon_area.dart';
+
+import '../layout/background.dart';
 
 class TradingScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Trading Screen'),
+    return Scaffold(
+      body: Stack(
+        children: [
+          // Background App
+          const BackgroundColor(),
+
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                // Space from top
+                const SizedBox(
+                  height: 55,
+                ),
+                TradingIconArea(),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
