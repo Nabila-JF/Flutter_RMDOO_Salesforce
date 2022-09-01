9d1c82559d5a9487d4d20f7b2611da197f3b7ab8
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-09-01 18:11:56 +0700

    moved quick info

---
+++ lib/widget/home/quick_info.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+class QuickInfo extends StatelessWidget{
+  const QuickInfo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 20, 0, 15),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(
+
+              children: const [
+                Text(
+                  'Last Transaction',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 12,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  '08/08/2022',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 14,
+                  ),
+                ),
+
+              ],
+            ),
+
+            Column(
+              children: const [
+                Text(
+                  'Average Spending',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 12,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  '220.456',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 14,
+                  ),
+                ),
+              ],
+            ),
+
+            Column(
+              children: const [
+                Text(
+                  'Point',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 12,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  '1057',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 14,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+  
+}
