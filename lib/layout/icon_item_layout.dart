c55bae7d5eabfb892e0fb9e2f2cc029e693e851e
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-09-03 13:35:17 +0700

    removed todo

--- lib/layout/icon_item_layout.dart
+++ lib/layout/icon_item_layout.dart
@@ -8,7 +8,6 @@ class IconItem extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Container(
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(15),
