5a2d89d24a529201b0385f9c7ddf536f52b44ef0
Nabila-JF <nabila060695@gmail.com>
2022-09-14 15:26:12 +0700

    Change icon shape from CircleAvatar to Container for adding a shadow

--- lib/widgets/appbar/default_subpages_appbar.dart
+++ lib/widgets/appbar/default_subpages_appbar.dart
@@ -3,21 +3,32 @@ import 'package:flutter/material.dart';
 // ignore: must_be_immutable
 class DefaultSubPagesAppBar extends StatelessWidget with PreferredSizeWidget {
   String pageTitle;
-   var pageSubtittle;
+  // ignore: prefer_typing_uninitialized_variables
+  var pageSubtittle;
 
-  DefaultSubPagesAppBar({super.key, required this.pageTitle, this.pageSubtittle});
+  DefaultSubPagesAppBar(
+      {super.key, required this.pageTitle, this.pageSubtittle});
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
       leading: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: CircleAvatar(
-          backgroundColor: Colors.white,
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            shape: BoxShape.circle,
+            boxShadow: [
+              BoxShadow(
+                  blurRadius: 10,
+                  color: Colors.black.withOpacity(0.15),
+                  spreadRadius: 0)
+            ],
+          ),
           child: Expanded(
             child: IconButton(
               icon: const Icon(
-                Icons.arrow_back,
+                Icons.arrow_back_rounded,
                 color: Color(0xff242F9B),
               ),
               onPressed: () {
